9a051354fbaad20ef690675f55bf0452ff944348
KNTXTremE <Kan.Horwang@gmail.com>
2018-12-18 20:31:51 +0700

    Add NodeMCU sources

---
+++ arduino/esp8266.ino
@@ -0,0 +1,125 @@
+#include <MicroGear.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "<<YOUR SSID>>";
+const char* password = "<<YOUR PASSWORD>>";
+int i = 0;
+String relay = "R0      ";
+String currentTime = "T17.00  ";
+
+#define APPID   "EmbeddedLab2018"
+#define KEY     "IJ40DCVoUQAXW23"
+#define SECRET  "tyyTIMnies37G3nTIfjCBjP45"
+
+#define BOARDALIAS   "esp8266"
+#define WEBALIAS   "HTML_web"
+WiFiClient client;
+
+int timer = 0;
+MicroGear microgear(client);
+
+/* If a new message arrives, do this */
+void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
+    Serial.print("Incoming message --> ");
+    msg[msglen] = '\0';
+    Serial.println((char *)msg);
+}
+
+void onFoundgear(char *attribute, uint8_t* msg, unsigned int msglen) {
+    Serial.print("Found new member --> ");
+    for (int i=0; i<msglen; i++)
+        Serial.print((char)msg[i]);
+    Serial.println();  
+}
+
+void onLostgear(char *attribute, uint8_t* msg, unsigned int msglen) {
+    Serial.print("Lost member --> ");
+    for (int i=0; i<msglen; i++)
+        Serial.print((char)msg[i]);
+    Serial.println();
+}
+
+/* When a microgear is connected, do this */
+void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) {
+    Serial.println("Connected to NETPIE...");
+    /* Set the alias of this microgear ALIAS */
+    microgear.setAlias(BOARDALIAS);
+}
+
+
+void setup() {
+    /* Add Event listeners */
+    /* Call onMsghandler() when new message arraives */
+    microgear.on(MESSAGE,onMsghandler);
+
+    /* Call onFoundgear() when new gear appear */
+    microgear.on(PRESENT,onFoundgear);
+
+    /* Call onLostgear() when some gear goes offline */
+    microgear.on(ABSENT,onLostgear);
+
+    /* Call onConnected() when NETPIE connection is established */
+    microgear.on(CONNECTED,onConnected);
+
+    Serial.begin(115200);
+    Serial.println("Starting...");
+
+    /* Initial WIFI, this is just a basic method to configure WIFI on ESP8266.                       */
+    /* You may want to use other method that is more complicated, but provide better user experience */
+    if (WiFi.begin(ssid, password)) {
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.print(".");
+        }
+    }
+
+    Serial.println("WiFi connected");  
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    /* Initial with KEY, SECRET and also set the ALIAS here */
+    microgear.init(KEY,SECRET,BOARDALIAS);
+
+    /* connect to NETPIE to a specific APPID */
+    microgear.connect(APPID);
+}
+
+void loop() {
+    /* To check if the microgear is still connected */
+    if (microgear.connected()) {
+//        Serial.println("connected");
+
+        /* Call this method regularly otherwise the connection may be lost */
+        microgear.loop();
+
+        if (Serial.available()) {
+//            Serial.println("Publish...");
+         if(i%2 == 0){
+              relay = "R0      ";
+              i++;
+         }
+         else if(i%2 == 1){
+              relay = "R1      ";
+              i++;
+         }
+              
+            
+            Serial.print(relay);
+            Serial.print(currentTime);
+            /* Chat with the microgear named ALIAS which is myself */
+//            microgear.chat(BOARDALIAS,"Hello");
+            
+            timer = 0;
+        } 
+        else timer += 100;
+    }
+    else {
+        Serial.println("connection lost, reconnect...");
+        if (timer >= 5000) {
+            microgear.connect(APPID);
+            timer = 0;
+        }
+        else timer += 100;
+    }
+    delay(1000);
+}
